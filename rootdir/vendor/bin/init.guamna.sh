a161ef75b2065c4043c3a6b01cffa284f8471539
electimon <electimon@gmail.com>
2023-03-03 15:20:30 +0800

    guamna: Fixup chromatix mounting
    Signed-off-by: electimon <electimon@gmail.com>

--- rootdir/vendor/bin/init.guamna.sh
+++ rootdir/vendor/bin/init.guamna.sh
@@ -4,3 +4,7 @@ device=$(getprop ro.boot.device)
 if [ "$device" = "guam" ]; then
     mount -o bind /vendor/lib64/com.qti.sensormodule.mot_guam_s5k3l6_ofilm.bin /vendor/lib64/camera/com.qti.sensormodule.mot_guam_s5k3l6_ofilm.bin
 fi
+
+if [ "$device" = "guamna" ] || [ "$device" = "guamp" ]; then
+    mount -o bind /vendor/lib64/camera/com.qti.sensormodule.mot_guamna_s5k3l6_ofilm.bin /vendor/lib64/camera/com.qti.sensormodule.mot_guam_s5k3l6_ofilm.bin
+fi
